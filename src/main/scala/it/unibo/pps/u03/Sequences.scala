24f192898d4b48b87fcf8e0d08aa685c108e1372
Aldo Visconti <aldo.visconti@studio.unibo.it>
2026-03-24 14:09:46 +0100

    Green Light for contains method

--- src/main/scala/it/unibo/pps/u03/Sequences.scala
+++ src/main/scala/it/unibo/pps/u03/Sequences.scala
@@ -123,7 +123,11 @@ object Sequences: // Essentially, generic linkedlists
      * E.g., [10, 20, 30] => true if elem is 20
      * E.g., [10, 20, 30] => false if elem is 40
      */
-    def contains[A](s: Sequence[A])(elem: A): Boolean = ???
+    @tailrec
+    def contains[A](s: Sequence[A])(elem: A): Boolean = s match
+      case Cons(head, tail) if head == elem => true
+      case Cons(_, tail) => contains(s)(elem)
+      case Nil() => false
 
     /*
      * Remove duplicates from the sequence
